15bf2591e1efc6b816537658d911cc8bfef87276
juliovianadev <juliovianadev@gmail.com>
2023-07-30 20:57:57 -0300

    graphql structure

---
+++ backend/index.ts
@@ -0,0 +1,18 @@
+import path from 'path'
+import {buildSchema} from 'type-graphql'
+import {ApolloServer} from 'apollo-server'
+
+async function main(){
+  const schema = await buildSchema({
+    resolvers: [],
+    emitSchemaFile: path.resolve(__dirname,'schema.gql'),
+  })
+
+  const server = new ApolloServer({
+    schema,
+  })
+
+  const {url} =await server.listen()
+  console.log("server is running on " ,url)
+}
+main()
