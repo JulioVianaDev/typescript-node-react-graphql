8908c189b2c6a2ec2645b1e3ec8790212f7ebbaa
juliovianadev <juliovianadev@gmail.com>
2023-07-30 21:26:34 -0300

    querys

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,14 @@
+import "reflect-metadata";
 import path from 'path'
 import {buildSchema} from 'type-graphql'
 import {ApolloServer} from 'apollo-server'
+import { UserResolver } from "./src/resolvers/userResolver";
 
 async function main(){
   const schema = await buildSchema({
-    resolvers: [],
+    resolvers: [
+      UserResolver
+    ],
     emitSchemaFile: path.resolve(__dirname,'schema.gql'),
   })
 
---
+++ backend/src/resolvers/userResolver.ts
@@ -0,0 +1,11 @@
+import { Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
+
+@Resolver()
+export class UserResolver{
+  private data: User[] = [];
+  @Query(()=>[User])
+  async hello(){
+    return this.data
+  }
+}
