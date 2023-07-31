bc8f71a77a837816e849b0f35bc4dba94af01985
juliovianadev <juliovianadev@gmail.com>
2023-07-30 21:33:52 -0300

    mutation

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -3,7 +3,7 @@ import { Field,ID,ObjectType } from "type-graphql";
 @ObjectType()
 export class User{
   @Field(_type=>ID)
-  id:string;
+  id: string;
 
   @Field()
   name: string;
--- backend/src/resolvers/userResolver.ts
+++ backend/src/resolvers/userResolver.ts
@@ -1,11 +1,18 @@
-import { Query, Resolver } from "type-graphql";
+import { Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../models/User";
 
 @Resolver()
 export class UserResolver{
   private data: User[] = [];
+
   @Query(()=>[User])
-  async hello(){
+  async users(){
     return this.data
+  }
+  @Mutation(()=>User)
+  async craeteUser(){
+    const user ={id: '1',name: "Tomate"}
+    this.data.push(user)
+    return user
   }
 }
